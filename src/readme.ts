3f23b4a0c891b93ac31937fad5bbbf533926a508
Özüm Eldoğan <ozum@ozum.net>
2020-05-10 16:14:39 +0300

    make oclif doc conditional

--- src/readme.ts
+++ src/readme.ts
@@ -1,9 +1,11 @@
 import Command from '@oclif/command'
 import execa = require('execa')
-import {readFile, ensureDir, move, remove} from 'fs-extra'
+import {readFile, ensureDir, move, remove, pathExists, readJSON} from 'fs-extra'
 import readmeasy from 'readmeasy'
 import typeDocSingleMd from './typedoc-single-md'
 import c from './node-bin'
+
+const DEFAULT_TEMPLATE_CONTENT = '{% include "module-header" %}\n\n# Synopsis\n\n# Details\n\n<!-- usage -->\n\n<!-- commands -->\n\n# API\n\n{% include "api.md" %}\n\n# Contribution\n\n'
 
 /**
  * Creates Readme from TypeScript and README template using readmeasy and Typeoc.
@@ -13,15 +15,23 @@ import c from './node-bin'
 export default async function readme(this: Command) {
   // if grep -q '{% include \"api.md\" %}' 'README.njk'; then npm run typedoc:single-md; mkdir -p temp && mv api.md temp/; fi && readmeasy --partial-dirs temp,/module-files/template-partials && rm -rf temp
   try {
-    const fileContent = await readFile('README.njk', {encoding: 'utf8'})
-    if (fileContent.includes('{% include "api.md" %}')) {
+    const templateExists = await pathExists('README.njk')
+    const fileContent = templateExists ? await readFile('README.njk', {encoding: 'utf8'}) : ''
+
+    if (!templateExists || fileContent.includes('{% include "api.md" %}')) {
       await typeDocSingleMd.apply(this)
       await ensureDir('temp')
-      await move('api.md', 'temp/api.md')
+      await move('api.md', 'temp/api.md', {overwrite: true})
     }
-    await readmeasy({partialDirs: ['temp', '/module-files/template-partials']})
+
+    await readmeasy({partialDirs: ['temp', '/module-files/template-partials'], defaultContent: DEFAULT_TEMPLATE_CONTENT})
     await remove('temp')
-    await execa(c('oclif-dev'), ['readme'], {stdio: 'inherit'})
+
+    // If oclif installed execute `oclif-dev readme`;
+    const oclifInstalled = Object.keys((await readJSON('package.json')).dependencies).includes('@oclif/command')
+    if (oclifInstalled) {
+      await execa(c('oclif-dev'), ['readme'], {stdio: 'inherit'})
+    }
   } catch (error) {
     this.error(error)
   }
--- src/typedoc-md.ts
+++ src/typedoc-md.ts
@@ -19,7 +19,7 @@ export default async function typedocMd(this: Command) {
   // rm -rf api-docs-md && typedoc --platform vuepress --plugin typedoc-plugin-example-tag,typedoc-plugin-markdown --excludeExternals --excludePrivate --excludeProtected --theme markdown --readme none --mode file --out api-docs-md && find api-docs-md -name \"index.md\" -exec sh -c 'mv \"$1\" \"${1%index.md}\"index2.md' - {} \\;
   try {
     await remove('api-docs-md')
-    await execa(c('typedoc'), ['--platform', 'vuepress', '--plugin', 'typedoc-plugin-example-tag,typedoc-plugin-markdown', '--excludeExternals', '--excludePrivate', '--excludeProtected', '--theme', 'markdown', '--readme', 'none', '--mode', 'file', '--out', 'api-docs-md'], {stdio: 'inherit'})
+    await execa(c('typedoc'), ['--platform', 'vuepress', '--plugin', 'typedoc-plugin-example-tag,typedoc-plugin-markdown', '--excludeExternals', '--excludePrivate', '--excludeProtected', '--excludeNotExported', '--theme', 'markdown', '--readme', 'none', '--mode', 'file', '--out', 'api-docs-md'], {stdio: 'inherit'})
     await execa('find', ['api-docs-md', '-name', '"index.md"', '-exec', 'sh', '-c', "'mv \"$1\" \"${1%index.md}\"index2.md'", '-', '{}', ';'], {stdio: 'inherit'}) // eslint-disable-line no-template-curly-in-string
   } catch (error) {
     this.error(error)
