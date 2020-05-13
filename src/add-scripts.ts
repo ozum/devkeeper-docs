5c084357a1689d5ad38baa027f6cc0a223b86f3f
Özüm Eldoğan <ozum@ozum.net>
2020-05-13 12:17:01 +0300

    fix cwd of add-scripts

--- src/add-scripts.ts
+++ src/add-scripts.ts
@@ -11,10 +11,10 @@ import c from './node-bin'
 export default async function typedocMd(this: Command) {
   // rm -rf api-docs-md && typedoc --platform vuepress --plugin typedoc-plugin-example-tag,typedoc-plugin-markdown --excludeExternals --excludePrivate --excludeProtected --theme markdown --readme none --mode file --out api-docs-md && find api-docs-md -name \"index.md\" -exec sh -c 'mv \"$1\" \"${1%index.md}\"index2.md' - {} \\;
   try {
-    await execa('npx', ['npm-add-script', '-k', 'readme', '-v', 'devkeeper-docs readme', '-f'], {stdio: 'inherit'})
+    await execa('npx', ['npm-add-script', '-k', 'readme', '-v', 'devkeeper-docs readme', '-f'], {stdio: 'inherit', cwd: process.env.$INIT_CWD})
     if (await pathExists('docs/.vuepress')) {
-      await execa('npx', ['npm-add-script', '-k', 'docs:build', '-v', 'devkeeper-docs vuepress', '-f'], {stdio: 'inherit'})
-      await execa('npx', ['npm-add-script', '-k', 'docs:dev', '-v', 'devkeeper-docs vuepress-dev', '-f'], {stdio: 'inherit'})
+      await execa('npx', ['npm-add-script', '-k', 'docs:build', '-v', 'devkeeper-docs vuepress', '-f'], {stdio: 'inherit', cwd: process.env.$INIT_CWD})
+      await execa('npx', ['npm-add-script', '-k', 'docs:dev', '-v', 'devkeeper-docs vuepress-dev', '-f'], {stdio: 'inherit', cwd: process.env.$INIT_CWD})
     }
   } catch (error) {
     this.error(error)
